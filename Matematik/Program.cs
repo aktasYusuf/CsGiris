cb441dd1a61b1c47b68fac0358ca4b27d03a55b3
YUSUF AKTAŞ <ysfaktas99@gmail.com>
2021-02-06 00:18:58 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(56,38);
+
+            dortIslem.Topla(87,53);
+        }
+    }
+}
